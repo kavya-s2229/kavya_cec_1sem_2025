0f86265bc69aba4b36ed0cb20159cc0ad8214562
kavya_2229 <ks218258@gmail.com>
2025-11-20 15:21:30 +0530

    day4 code pushed

---
+++ day 4/3_varibles.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age = 30;
+    printf("Your age is:%d",age);
+
+    float marks = 345.34;
+    printf("\nYour marks is:%f",marks);
+
+    double cgpa =9.345;
+    printf("\nYour cgpa is:%lf",cgpa);
+
+    char name ='k';
+    printf("\nYour name is:%c",name); 
+
+    bool iswalking = false;
+    printf("\n is\" walk\ting\":%d",iswalking);
+}
