c626f7f383c127f0031f6ec7689410d9e37819f2
kavya_2229 <ks218258@gmail.com>
2025-11-27 14:47:40 +0530

    dsa

---
+++ day9/3_bubble.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int arr[50], size = 0;
+    printf("Enter a size of array:\n");
+    scanf("%d", &size);
+    if (size > 50)
+    {
+        printf("Array out of bound\n");
+    }
+    else
+    {
+        printf("Enter elements:\n");
+        for (int i = 0; i < size; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j<size-i-1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printf("Array Element:\n");
+        for(int i=0;i<size;i++){
+        printf("%d",arr[i]);
+        }
+    }
+}
